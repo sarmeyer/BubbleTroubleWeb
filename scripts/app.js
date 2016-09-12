3506cb031ea1e35f27322e32f59b0bb8e59b316e
Sarah <sarahmichellemeyer@gmail.com>
2016-09-12 10:44:02 -0600

    updates to responsiveness

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 var app = angular.module('game', ['ngRoute']);
 
 app.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'partials/home.html'
-        })
-        .when('/game', {
-            templateUrl: 'partials/game.html'
-        })
-        .when('/about', {
-            templateUrl: 'partials/about.html'
-        })
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html'
+    })
+    .when('/game', {
+      templateUrl: 'partials/game.html'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html'
+    })
 })
